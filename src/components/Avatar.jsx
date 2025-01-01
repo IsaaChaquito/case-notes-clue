c85708e0b46d0142067693e810d00960e4081453
Isaac Castro G <87621852+IsaaChaquito@users.noreply.github.com>
2025-01-01 11:28:23 -0600

    avatars width

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -11,8 +11,8 @@ export const color = [
 export const Avatar = ({ index }) => {
   return (
     <div className="w-full flex items-center justify-center">
-      <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-          <svg className={`absolute w-12 h-12 ${color[index]} -left-1`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path></svg>
+      <div className="relative w-8 h-8 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+          <svg className={`absolute w-10 h-10 ${color[index]} -left-1`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path></svg>
       </div>
     </div>
   )
