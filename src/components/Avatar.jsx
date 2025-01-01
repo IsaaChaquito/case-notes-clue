ffe36e4f5ac051241fd6d6092b4d918bafc269d1
Isaac Castro G <87621852+IsaaChaquito@users.noreply.github.com>
2025-01-01 11:53:57 -0600

    fixing select color for avatar, now can select verduzco

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,46 +1,61 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export const color = [
-                "bg-green-500", 
-                "bg-yellow-500", 
-                "bg-blue-500", 
-                "bg-purple-500", 
-                "bg-red-500", 
-                "bg-white dark:bg-black"
-              ]
+  "bg-green-500",
+  "bg-yellow-500",
+  "bg-blue-500",
+  "bg-purple-500",
+  "bg-red-500",
+  "bg-white dark:bg-black",
+];
 
-export const colorNames = ["Verduzco", "Mostaza", "Marlene", "Moradillo", "Escarlata", "Blanca"]
+export const colorNames = [
+  "Verduzco",
+  "Mostaza",
+  "Marlene",
+  "Moradillo",
+  "Escarlata",
+  "Blanca",
+];
 
 export const Avatar = ({ index }) => {
+  const [avatarColorIndex, setAvatarColorIndex] = useState(index);
 
-  const [avatarColor, setAvatarColor] = useState(color[index])
   const handleColor = (e) => {
-    console.log(e.target.value);
-    setAvatarColor(color[e.target.value])
-  }
-
+    const newIndex = parseInt(e.target.value, 10); // Convertir a número
+    setAvatarColorIndex(newIndex);
+  };
 
   return (
     <div className="relative w-full flex items-center justify-center">
       <div className="relative w-8 h-8 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-          <svg className={`absolute w-10 h-10 ${avatarColor} -left-1`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path></svg>
+        <svg
+          className={`absolute w-10 h-10 ${color[avatarColorIndex]} -left-1`}
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+            clipRule="evenodd"
+          ></path>
+        </svg>
       </div>
 
-      <select className="absolute top-0 left-0 w-full h-full opacity-0 dark:bg-black" value={avatarColor} onChange={handleColor} >
-        {
-            color.map((_, index) => (
-              <option 
-                className="z-50" 
-                key={index} 
-                value={ index } 
-              >
-                { colorNames[index] + " " + index }
-              </option>
-            ))
-          }
+      <select
+        className="absolute top-0 left-0 w-full h-full opacity-0 dark:bg-black"
+        value={avatarColorIndex} // Usar índice aquí
+        onChange={handleColor}
+      >
+        {color.map((_, idx) => (
+          <option className="z-50" key={idx} value={idx}>
+            {colorNames[idx] + " " + idx}
+          </option>
+        ))}
       </select>
     </div>
-  )
-}
+  );
+};
 
-export default Avatar
+export default Avatar;
