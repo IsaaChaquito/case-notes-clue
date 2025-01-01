d9de48e73aeb83c239f383936ea06f9337f0d9af
Isaac Castro G <87621852+IsaaChaquito@users.noreply.github.com>
2025-01-01 01:47:07 -0600

    improving line-through logic

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -10,26 +10,13 @@ export const Row = (
   }
 ) => {
 
-  const labelStates = { 
-    noUnderline: 'no-underline', 
-    lineThrough: 'line-through decoration-8'
-  }
-
-  const [labelState, setLabelState] = useState('no-underline')
-
-  const alternateUnderline = () => {
-    if (labelState === labelStates.noUnderline) {
-      setLabelState(labelStates.lineThrough)
-    } else {
-      setLabelState(labelStates.noUnderline)
-    } 
-  }
+  const [labelLineThrough, setLabelLineThrough] = useState(false)
 
   return (
     <div className="CLUE-SUSPECTS h-7 flex items-center select-none">
       <h1 
-        onClick={ alternateUnderline }
-        className={`min-w-28 h-full text-black text-sm text-nowrap text-center cursor-pointer  ${labelColor} ${labelState} p-1 `}>
+        onClick={ () => setLabelLineThrough(!labelLineThrough) }
+        className={`min-w-28 h-full text-black text-sm text-nowrap text-center cursor-pointer  ${labelColor} ${labelLineThrough ? 'line-through decoration-8' : 'no-underline'} p-1 `}>
         {label}
       </h1>
       {
