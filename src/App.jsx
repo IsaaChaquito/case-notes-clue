9599dea7782d8603c98b43f153116d6d4ffa0a5c
Isaac Castro G <87621852+IsaaChaquito@users.noreply.github.com>
2024-12-31 18:50:55 -0600

    restoring check-box icon

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   }
 
   return (
-    <div className="sm:min-w-screen min-h-screen bg-white dark:bg-black/70 flex flex-col items-center gap-y-1">
+    <div className="sm:min-w-screen min-h-screen bg-white dark:bg-black flex flex-col items-center gap-y-1">
 
       <div className='flex justify-between items-center m-1 gap-x-2'>
         <LogoClueIcon className='w-36 h-14 text-black dark:text-white' />
