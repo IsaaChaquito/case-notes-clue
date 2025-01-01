1c29492ae37a9201a611aea8857f56367aec8223
Isaac Castro G <87621852+IsaaChaquito@users.noreply.github.com>
2024-12-31 19:31:39 -0600

    fine-tunning colors

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,12 +14,12 @@ export const Card = ({ numberOfPlayers = 6 }) => {
           Sospechosos
         </h2>
 
-        <Row label="Verduzco" labelColor="bg-green-500" numberOfOptions={numberOfPlayers} />
-        <Row label="Mostaza" labelColor="bg-yellow-500" numberOfOptions={numberOfPlayers} />
-        <Row label="Marlene" labelColor="bg-blue-500" numberOfOptions={numberOfPlayers} />
-        <Row label="Moradillo" labelColor="bg-purple-500" numberOfOptions={numberOfPlayers} />
-        <Row label="Escarlata" labelColor="bg-red-500" numberOfOptions={numberOfPlayers} />
-        <Row label="Blanca" labelColor="bg-white text-black" numberOfOptions={numberOfPlayers} />
+        <Row label="Verduzco" labelColor="border-x-2 border-b-2 border-gray-700 text-green-500" numberOfOptions={numberOfPlayers} />
+        <Row label="Mostaza" labelColor="border-x-2 border-b-2 border-gray-700 text-yellow-500" numberOfOptions={numberOfPlayers} />
+        <Row label="Marlene" labelColor="border-x-2 border-b-2 border-gray-700 text-blue-500" numberOfOptions={numberOfPlayers} />
+        <Row label="Moradillo" labelColor="border-x-2 border-b-2 border-gray-700 text-purple-500" numberOfOptions={numberOfPlayers} />
+        <Row label="Escarlata" labelColor="border-x-2 border-b-2 border-gray-700 text-red-500" numberOfOptions={numberOfPlayers} />
+        <Row label="Blanca" labelColor="border-x-2 border-b-2 border-gray-700 text-black dark:text-white" numberOfOptions={numberOfPlayers} />
       </section>
 
       <section className="SUSPECTS w-full h-auto">
@@ -27,12 +27,12 @@ export const Card = ({ numberOfPlayers = 6 }) => {
           Armas
         </h2>
 
-        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-white" label="Candelabro" numberOfOptions={numberOfPlayers} />
-        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-white" label="Daga" numberOfOptions={numberOfPlayers} />
-        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-white" label="Tubo de plomo" numberOfOptions={numberOfPlayers} />
-        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-white" label="Revólver" numberOfOptions={numberOfPlayers} />
-        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-white" label="Soga" numberOfOptions={numberOfPlayers} />
-        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-white" label="Llave inglesa" numberOfOptions={numberOfPlayers} />
+        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-black dark:text-white" label="Candelabro" numberOfOptions={numberOfPlayers} />
+        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-black dark:text-white" label="Daga" numberOfOptions={numberOfPlayers} />
+        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-black dark:text-white" label="Tubo de plomo" numberOfOptions={numberOfPlayers} />
+        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-black dark:text-white" label="Revólver" numberOfOptions={numberOfPlayers} />
+        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-black dark:text-white" label="Soga" numberOfOptions={numberOfPlayers} />
+        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-black dark:text-white" label="Llave inglesa" numberOfOptions={numberOfPlayers} />
       </section>
 
       <section className="SUSPECTS w-full h-auto">
@@ -40,15 +40,15 @@ export const Card = ({ numberOfPlayers = 6 }) => {
           Lugares
         </h2>
 
-        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-white" label="Salón de baile" numberOfOptions={numberOfPlayers} />
-        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-white" label="Sala de billar" numberOfOptions={numberOfPlayers} />
-        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-white" label="Terraza" numberOfOptions={numberOfPlayers} />
-        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-white" label="Comedor" numberOfOptions={numberOfPlayers} />
-        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-white" label="Pasillo" numberOfOptions={numberOfPlayers} />
-        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-white" label="Cocina" numberOfOptions={numberOfPlayers} />
-        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-white" label="Biblioteca" numberOfOptions={numberOfPlayers} />
-        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-white" label="Sala" numberOfOptions={numberOfPlayers} />
-        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-white" label="Estudio" numberOfOptions={numberOfPlayers} />
+        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-black dark:text-white" label="Salón de baile" numberOfOptions={numberOfPlayers} />
+        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-black dark:text-white" label="Sala de billar" numberOfOptions={numberOfPlayers} />
+        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-black dark:text-white" label="Terraza" numberOfOptions={numberOfPlayers} />
+        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-black dark:text-white" label="Comedor" numberOfOptions={numberOfPlayers} />
+        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-black dark:text-white" label="Pasillo" numberOfOptions={numberOfPlayers} />
+        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-black dark:text-white" label="Cocina" numberOfOptions={numberOfPlayers} />
+        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-black dark:text-white" label="Biblioteca" numberOfOptions={numberOfPlayers} />
+        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-black dark:text-white" label="Sala" numberOfOptions={numberOfPlayers} />
+        <Row labelColor="border-x-2 border-b-2 border-gray-700 text-black dark:text-white" label="Estudio" numberOfOptions={numberOfPlayers} />
       </section>
     </div>
   )
