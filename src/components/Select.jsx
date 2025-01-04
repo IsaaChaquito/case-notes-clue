a7a330ac2a13a104d9565d3e07e46b34ec3d4cfb
Isaac Castro G <87621852+IsaaChaquito@users.noreply.github.com>
2025-01-04 12:09:55 -0600

    pading x custom select

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -5,7 +5,7 @@ export const Select = ({ title, numberOfItems, MAX_ITEMS, handler }) => {
     <select 
       onChange={(e) => handler(e)} 
       value={numberOfItems}
-      className='w-auto h-7 px-0.5 text-white text-xs bg-black dark:text-black dark:bg-white rounded outline-none'
+      className='w-auto h-7 px-1 text-white text-xs bg-black dark:text-black dark:bg-white rounded outline-none'
     >
       {
         Array.from({  length: MAX_ITEMS }).map((_, index) => (
