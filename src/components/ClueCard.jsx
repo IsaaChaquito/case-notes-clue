db57f659ce45f4a925ac3706932903683f4588e2
Isaac Castro G <87621852+IsaaChaquito@users.noreply.github.com>
2025-01-05 01:03:51 -0600

    text-white in any mode

--- src/components/ClueCard.jsx
+++ src/components/ClueCard.jsx
@@ -16,7 +16,7 @@ export const ClueCard = ({ name, onClose }) => {
   return (
     <div 
       onClick={ onShowContent }
-      className={`CARD-IMAGE  fixed left-0 top-0 bottom-0 right-0  p-2  bg-blue-950 rounded-sm text-xl text-black dark:text-white flex justify-center items-center duration-150 z-50 animate-fade-in`}>
+      className={`CARD-IMAGE  fixed left-0 top-0 bottom-0 right-0  p-2  bg-blue-950 rounded-sm text-xl text-white flex justify-center items-center duration-150 z-50 animate-fade-in`}>
       
       <button onClick={ onCloseClueCard } className="absolute top-2 right-2 p-2 rounded-full bg-white text-black">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
